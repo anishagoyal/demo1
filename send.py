04223d113897c627e5185b6d46bd1ac0c000aab3
anishagoyal <anisha.gyl95@gmail.com>
2017-06-12 15:29:24 +0530

    sending msg

---
+++ send.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2
+
+import  socket
+
+s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+server_ip="192.168.1.8"
+server_port=9999
+
+
+while True :
+	msg=raw_input("enter your message :    ")
+	s.sendto(msg,(server_ip,server_port))	
+	print  s.recvfrom(100)
